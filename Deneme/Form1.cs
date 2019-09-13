3ef4c02b9312cb8fa0dcde33a421f1a2054451b8
Tayfur26 <Tayfur26@users.noreply.github.com>
2019-09-13 17:59:40 +0300

    CSharp

---
+++ Deneme/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sha = new SHA256Managed();
+            SHA256 sifrele = new SHA256CryptoServiceProvider();
+            string SifrelenecekMetin = textBox1.Text;
+            string SifreliVeri = Convert.ToBase64String(sifrele.ComputeHash(Encoding.UTF8.GetBytes(SifrelenecekMetin)));
+            var bytess = System.Text.Encoding.UTF8.GetBytes(SifreliVeri);
+            var longValue = BitConverter.ToInt64(bytess, 0);
+            textBox2.Text = SifreliVeri;
+            textBox3.Text = Convert.ToString(longValue);
+            
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.Filter = "Tüm Dosyalar |*.*| Resim Dosyası |*.jpg;*.nef;*.png| Video|*.avi";
+            dosya.Title = "Dosya Seç";
+            dosya.ShowDialog();
+            string DosyaYolu = dosya.FileName;
+            textBox1.Text = dosya.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetChecksum(textBox1.Text);
+            textBox3.Text = GetChecksum1(textBox1.Text);
+            textBox4.Text = GetChecksum2(textBox1.Text);
+        }
+
+        public static string GetChecksum(string file)
+        {
+            using (FileStream stream = File.OpenRead(file))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+                
+            }
+        }
+        public static string GetChecksum1(string file)
+        {
+            using (FileStream stream = File.OpenRead(file))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+                var longValue = BitConverter.ToInt64(checksum, 0);
+                return longValue.ToString();
+
+            }
+        }
+        public static string GetChecksum2(string file)
+        {
+            using (FileStream stream = File.OpenRead(file))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+
+                return checksum.ToString();
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int boy = textBox2.Text.Length;
+            label7.Text = boy.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show(); 
+            Form1 f1 = new Form1();
+            f1.Close();
+            this.Hide();
+        }
+    }
+}
---
+++ Deneme/Form2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deneme
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public static class Ortak
+        {
+            public static int fi_n { get; set; }
+            public static int e_ { get; set; }
+           
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int p = Convert.ToInt32(textBox1.Text);
+            int q = Convert.ToInt32(textBox2.Text);
+            if (isprime(p) == 0 || isprime(q) == 0)
+            {
+                MessageBox.Show("Sayılar Asal olmalıdır.");
+            }
+            else 
+            {
+                int n = p * q;
+                Ortak.fi_n= (p - 1) * (q - 1);
+                for (int i = 0; i <= 999999999; i++)
+                {
+                    Ortak.e_ = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Fi(n) ile aralarında asal bir sayı giriniz", "e sayısı seçme", "", 10, 10));
+                    if (a_asal(Ortak.e_, Ortak.fi_n) == 1)
+                        break;
+                    else 
+                    {
+                        MessageBox.Show("e sayısı ile Fi(n) aralarında asal olmalıdır.");
+                    }
+                }
+                int d = ters(Ortak.e_, Ortak.fi_n);
+                
+                textBox7.Text = Convert.ToString(d);
+                textBox8.Text = Convert.ToString(Ortak.fi_n);
+                 textBox5.Text = Convert.ToString(Ortak.e_);
+                 textBox6.Text = Convert.ToString(Ortak.fi_n);
+                
+                
+            }
+        }
+        public static double isprime(double sayi)
+        {
+            if (sayi == 1 || sayi < 1)
+            {
+                MessageBox.Show("Girilen sayı 1 den büyük olmalıdır.");
+                return 0;
+            }
+            else
+            {
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                        return 0;
+                }
+               
+              
+                return 1;
+            
+            }
+        }
+        public static int a_asal(int a, int b)
+        {
+
+            
+            int min = a;
+
+            if (b < min) min = b;
+
+            for (int i = 2; i <= min; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+
+                    return 0; 
+
+                }
+                else if (i == min)
+
+                { return 1; }
+
+            }
+            return 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Close();
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        public static int ters(int e, int fi)
+        {
+            double result;
+            int k = 1;
+            while (true)
+            {
+                result = (1 + (k * fi)) / (double)e;
+                if ((Math.Round(result, 5) % 1) == 0) //integer
+                {
+                    return (int)result;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
